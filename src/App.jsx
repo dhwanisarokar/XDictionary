b5ab5e8a402c2ccbf56076062930c6c3a5c762b8
dhwanisarokar <dhwanisarokar@gmail.com>
2024-04-06 22:20:07 +0530

    impliment words dictinary logic

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import dictionary from "../constants/dictionary";
+import "./App.css";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    setMessage("");
+
+    if (search) {
+      const definition = dictionary.find(
+        (word) => word.word.toLocaleLowerCase() === search.toLocaleLowerCase()
+      ) || {
+        meaning: "Word not found in the dictionary.",
+      };
+      // console.log(definition.meaning);
+      setMessage(definition.meaning);
+    }
+  };
+
+  return (
+    <>
+      <h2>Dictionary App</h2>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          name="search"
+          placeholder="Search for a word..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button typeof="submit">Search</button>
+      </form>
+      <h4>Definition: </h4>
+      <p>{message}</p>
+    </>
+  );
+}
+
+export default App;
